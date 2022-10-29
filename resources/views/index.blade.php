543deed8a28bb27b51f714ad1eac9677488d737d
Fauzan Mustofa <fauzan.mustofa@binus.ac.id>
2022-10-29 23:39:11 +0700

    Update index.blade.php
    Update newsletter

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -197,12 +197,12 @@
             <div class="container">
                 <div class="border rounded border-white d-flex flex-column justify-content-between align-items-center flex-lg-row bg-primary-gradient p-4 p-lg-5" style="background: #E9F5DB;">
                     <div class="text-center text-lg-start py-3 py-lg-1">
-                        <h2 class="fw-bold mb-2">Subscribe to our newsletter</h2>
-                        <p class="mb-0">Imperdiet consectetur dolor.</p>
+                        <h2 class="fw-bold mb-2">Berlangganan newsletter kami</h2>
+                        <p class="mb-0">Untuk mendapatkan info, promo dan hal menarik lainnya.</p>
                     </div>
                     <form class="d-flex justify-content-center flex-wrap flex-lg-nowrap" method="post">
                         <div class="my-2"><input class="border rounded-pill shadow-sm form-control" type="email" name="email" placeholder="Your Email"></div>
-                        <div class="my-2"><button class="btn btn-primary shadow ms-2" type="submit" style="background: #01AA4F;border-style: none;">Subscribe </button></div>
+                        <div class="my-2"><button class="btn btn-primary shadow ms-2" type="submit" style="background: #01AA4F;border-style: none;">Berlangganan </button></div>
                     </form>
                 </div>
             </div>
