0e270d80eee8a274bcb789f60498b8a971fbc081
Thomas Harman Bintang <thomasharman99@gmail.com>
2022-12-06 16:49:01 +0700

    Tambah Tabel Admin dan Station

---
+++ database/migrations/2022_12_06_091952_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->bigIncrements('id_admin');
+            $table->string('email');
+            $table->string('password');
+            $table->string('nama_admin');
+            $table->string('jabatan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
---
+++ database/migrations/2022_12_06_094412_create_stations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->string('nomor_seri')->primary();
+            $table->string('lokasi');
+            $table->string('produk');
+            $table->integer('kapasitas_tangki');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stations');
+    }
+};
