7cdfcb4f6a94912ec1649e646929511d88b3624f
MindCl0ud <ahmadagilsusanto@gmail.com>
2022-12-13 16:08:59 +0700

    menambahkan route di contacs dan services

--- resources/views/landing/contacts.blade.php
+++ resources/views/landing/contacts.blade.php
@@ -23,8 +23,8 @@
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ url('services')}}">Services</a></li>
-                    <li class="nav-item"><a class="nav-link  active" href="{{ url('contacts')}}">Contacts</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('services')}}">Services</a></li>
+                    <li class="nav-item"><a class="nav-link  active" href="{{ route('contacts')}}">Contacts</a></li>
                 </ul><a class="btn btn-primary shadow" role="button" href="{{ route('daftar') }}" style="background: #01AA4F;border-style: none;">Daftar</a>
             </div>
         </div>
