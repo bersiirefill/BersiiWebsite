450077b12b57aa7f1aac6e168d662773f3a0c7b8
Hanustavira Guru Acarya <hanustavira.acarya@binus.ac.id>
2022-12-19 21:32:32 +0700

    Cek newsletter

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -225,12 +225,49 @@
                 <div class="border rounded border-white d-flex flex-column justify-content-between align-items-center flex-lg-row bg-primary-gradient p-4 p-lg-5" style="background: #E9F5DB;">
                     <div class="text-center text-lg-start py-3 py-lg-1">
                         <h2 class="fw-bold mb-2">Berlangganan newsletter kami</h2>
-                        <p class="mb-0">Untuk mendapatkan info, promo dan hal menarik lainnya.</p>
+                        <p class="mb-0">Dapatkan info, promo dan hal menarik lainnya.</p>
                     </div>
-                    <form class="d-flex justify-content-center flex-wrap flex-lg-nowrap" method="post">
-                        <div class="my-2"><input class="border rounded-pill shadow-sm form-control" type="email" name="email" placeholder="Email Kamu"></div>
-                        <div class="my-2"><button class="btn btn-primary shadow ms-2" type="submit" style="background: #01AA4F;border-style: none;">Berlangganan </button></div>
+                    <form class="d-flex justify-content-center flex-wrap flex-lg-nowrap" >
+
+                        {{-- <div class="my-2"><input class="border rounded-pill shadow-sm form-control" type="email" name="email" placeholder="Email Kamu"></div>
+                        <div class="my-2"><button class="btn btn-primary shadow ms-2" type="submit" style="background: #01AA4F;border-style: none;">Berlangganan </button></div> --}}
                     </form>
+
+<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-071822.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background: #fff; clear: right;; font:16px Inter,Helvetica,Arial,sans-serif;  width:600px;}
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+    <form action="https://bersii.us17.list-manage.com/subscribe/post?u=b2e60aa8ae0f43f586f05efeb&amp;id=f88a78bfec&amp;f_id=002f4ce0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+        {{-- <h2>Berlangganan newsletter kami</h2> --}}
+        <div class="indicates-required"></div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">
+</label>
+	<input type="email" value="" name="EMAIL" class="required email border rounded-pill shadow-sm" id="mce-EMAIL" placeholder="Email Kamu" required>
+	<span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+</div>
+	<div id="mce-responses" class="clear foot">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b2e60aa8ae0f43f586f05efeb_f88a78bfec" tabindex="-1" value=""></div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" value="Berlangganan" name="subscribe" id="mc-embedded-subscribe" class="button btn-primary shadow rounded-pill" style="background: #01AA4F">
+                {{-- <p class="brandingLogo"><a href="http://eepurl.com/igvKZn" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p> --}}
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
+
                 </div>
             </div>
         </section>
