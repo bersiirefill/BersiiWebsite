5eae3021c06fce2e5a2bc8569e005ae7e208cbaf
Hanustavira Guru Acarya <hanustavira.acarya@binus.ac.id>
2023-01-16 07:57:46 +0700

    Update sql export

--- bersii.sql
+++ bersii.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Des 2022 pada 04.56
+-- Waktu pembuatan: 15 Jan 2023 pada 21.51
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.1.12
 
@@ -42,7 +42,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id_admin`, `email`, `password`, `nama`, `jabatan`, `created_at`, `updated_at`) VALUES
-('780570', 'bersiirefill@gmail.com', '$2y$10$vVmrVmzuFf8YX1QaLo7ZB.N7jspwtOTOtManiqQlznxGPtD32/Fb.', 'Admin Bersii', 'Administrator', '2022-12-27 01:20:00', '2022-12-27 01:20:00');
+('303507', 'bersiirefill@gmail.com', '$2y$10$IeQvF427TH9izXZK.92VY.mX9WDeMA7dYeyK9wqiKeyhWoLNue3De', 'Admin Bersii', 'Administrator', '2023-01-09 20:12:20', '2023-01-09 20:12:20');
 
 -- --------------------------------------------------------
 
@@ -54,7 +54,7 @@ CREATE TABLE `isi_refill` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `id_produk` varchar(191) NOT NULL,
   `nomor_seri` varchar(191) NOT NULL,
-  `stok` bigint(20) DEFAULT NULL,
+  `stok` double DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -64,7 +64,7 @@ CREATE TABLE `isi_refill` (
 --
 
 INSERT INTO `isi_refill` (`id`, `id_produk`, `nomor_seri`, `stok`, `created_at`, `updated_at`) VALUES
-(2, 'PRD-1974327192', '00000000c0ed5881', 13, '2022-12-27 22:32:09', '2022-12-27 22:32:09');
+(1, 'PRD-415051578', '00000000c0ed5881', 0.5, '2023-01-12 23:31:51', '2023-01-12 23:31:51');
 
 -- --------------------------------------------------------
 
@@ -76,7 +76,8 @@ CREATE TABLE `jenis_refill_riwayat` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `id_trx` varchar(191) NOT NULL,
   `id_produk` varchar(191) NOT NULL,
-  `jumlah` bigint(20) DEFAULT NULL,
+  `jumlah_refill` double DEFAULT NULL,
+  `harga` bigint(20) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -89,7 +90,7 @@ CREATE TABLE `jenis_refill_riwayat` (
 
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(191) NOT NULL,
+  `migration` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -134,10 +135,11 @@ CREATE TABLE `personal_access_tokens` (
 --
 
 INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
-(1, 'App\\Models\\Admin', '780570', '00000000c0ed5881', '9a9e1a74d6d4f6c66e835c799d708162e41faf6a532f72ba4cb8348dd422bef9', '[\"*\"]', '2022-12-28 03:52:02', NULL, '2022-12-28 03:05:41', '2022-12-28 03:52:02'),
-(2, 'App\\Models\\Admin', '780570', '00000000c0ed5881', 'f6f47f023b3920c6e9d2ed679af428a1432d5701288901030aff98afe488bf83', '[\"*\"]', '2022-12-28 03:44:09', NULL, '2022-12-28 03:44:09', '2022-12-28 03:44:09'),
-(3, 'App\\Models\\Admin', '780570', '00000000c0ed5881', 'b1f4682f80d0157957cf289a0cc4284baa8e5b07a4fc8ad5abce4675ea1fd991', '[\"*\"]', '2022-12-28 03:51:57', NULL, '2022-12-28 03:51:57', '2022-12-28 03:51:57'),
-(4, 'App\\Models\\Admin', '780570', '00000000c0ed5881', '3bc9b8845939179dede7485d8db56d8c957412e1207baf7e020535b2e68ef5b6', '[\"*\"]', NULL, NULL, '2022-12-28 03:52:10', '2022-12-28 03:52:10');
+(1, 'App\\Models\\Admin', '303507', '00000000c0ed5881', '16e1bf446952aa8b40209086f99fdb8ec3de46b01f441e50f5b8b2088bbf7639', '[\"*\"]', '2023-01-14 21:26:50', NULL, '2023-01-12 23:25:57', '2023-01-14 21:26:50'),
+(2, 'App\\Models\\User', '882536', '00000000c0ed5881', '24f4ff3c7c19fd37080f12dea222ecc55ef6de1779f41580d70339860fd6c6bb', '[\"*\"]', '2023-01-14 20:19:39', NULL, '2023-01-14 20:19:38', '2023-01-14 20:19:39'),
+(3, 'App\\Models\\User', '882536', '00000000c0ed5881', '846084fa56d6698df79e739125bc22a01b8283b1d748167f4caeb1ee4578eac9', '[\"*\"]', '2023-01-14 20:27:24', NULL, '2023-01-14 20:20:56', '2023-01-14 20:27:24'),
+(4, 'App\\Models\\User', '882536', '00000000c0ed5881', '7f859410f0a3070f0bd19a0029d58ca74109a4e6d0f215be871eb764428bb283', '[\"*\"]', '2023-01-14 20:28:05', NULL, '2023-01-14 20:28:04', '2023-01-14 20:28:05'),
+(5, 'App\\Models\\User', '882536', '00000000c0ed5881', '70e8e86904be3c938d16fa1f1f6eb4009e3231bf108077ff1c3b1f66567c5fe2', '[\"*\"]', '2023-01-14 21:43:24', NULL, '2023-01-14 20:30:20', '2023-01-14 21:43:24');
 
 -- --------------------------------------------------------
 
@@ -149,7 +151,10 @@ CREATE TABLE `produk_supplier` (
   `id_produk` varchar(191) NOT NULL,
   `kode_supplier` varchar(191) DEFAULT NULL,
   `nama_produk` varchar(191) DEFAULT NULL,
-  `stok` bigint(20) DEFAULT NULL,
+  `deskripsi_produk` varchar(191) DEFAULT NULL,
+  `stok` double DEFAULT NULL,
+  `harga_produk` bigint(20) DEFAULT NULL,
+  `gambar_produk` varchar(191) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -158,8 +163,8 @@ CREATE TABLE `produk_supplier` (
 -- Dumping data untuk tabel `produk_supplier`
 --
 
-INSERT INTO `produk_supplier` (`id_produk`, `kode_supplier`, `nama_produk`, `stok`, `created_at`, `updated_at`) VALUES
-('PRD-1974327192', 'BSPL-349', 'So Wangi', 55, '2022-12-27 01:35:39', '2022-12-27 22:32:09');
+INSERT INTO `produk_supplier` (`id_produk`, `kode_supplier`, `nama_produk`, `deskripsi_produk`, `stok`, `harga_produk`, `gambar_produk`, `created_at`, `updated_at`) VALUES
+('PRD-415051578', 'BSPL-784', 'Minyak Goreng Hatiku', 'Jagonya lengo', 0.5, 25000, 'PRD-415051578.jpg', '2023-01-09 21:24:17', '2023-01-12 23:31:51');
 
 -- --------------------------------------------------------
 
@@ -182,7 +187,7 @@ CREATE TABLE `refill_stations` (
 --
 
 INSERT INTO `refill_stations` (`nomor_seri`, `latitude`, `longitude`, `status_mesin`, `alamat`, `created_at`, `updated_at`) VALUES
-('00000000c0ed5881', '-7.9666204', '112.6326321', '0', 'Singosari', '2022-12-27 01:21:17', '2022-12-28 03:06:31');
+('00000000c0ed5881', '-7.9666204', '112.6326321', '0', 'Singosari', '2023-01-09 20:12:20', '2023-01-09 20:12:20');
 
 -- --------------------------------------------------------
 
@@ -194,6 +199,7 @@ CREATE TABLE `riwayat_refills` (
   `id_trx` varchar(191) NOT NULL,
   `nomor_seri` varchar(191) NOT NULL,
   `id_user` varchar(191) NOT NULL,
+  `total_harga` bigint(20) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -237,8 +243,8 @@ CREATE TABLE `suppliers` (
 --
 
 INSERT INTO `suppliers` (`kode_supplier`, `nama_pemilik`, `nama_toko`, `nomor_telepon`, `alamat`, `created_at`, `updated_at`) VALUES
-('BSPL-349', 'Ahmad Muzakki', 'Maju Jaya Sentosa Abadi Lancar', '08123456', 'Sawojajar', '2022-12-27 01:20:00', '2022-12-27 04:49:58'),
-('BSPL-709', 'Fauzan Mustofa', 'Sumber Selamat', '08123456', 'Singosari', '2022-12-27 01:20:00', '2022-12-27 01:20:00');
+('BSPL-784', 'Fauzan Mustofa', 'Sumber Selamat', '08123456', 'Singosari', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('BSPL-839', 'Ahmad Muzakki', 'Maju Jaya Sentosa Abadi Lancar', '08123456', 'Sawojajar', '2023-01-09 20:12:20', '2023-01-09 20:12:20');
 
 -- --------------------------------------------------------
 
@@ -264,11 +270,11 @@ CREATE TABLE `users_bersiis` (
 --
 
 INSERT INTO `users_bersiis` (`id`, `nama`, `email`, `nomor_telepon`, `alamat`, `password`, `forgot_token`, `remember_token`, `created_at`, `updated_at`) VALUES
-('449217', 'Achmad Agil Susanto', 'ahmadagilsusanto@gmail.com', '6282237174435', 'Sawojajar', '$2y$10$6mlty3efWKQ2xwxwHeE7g.5jTZ2ay5rgN6PxblxUZ7N4kd52zbh46', NULL, 'JVoPUImi16FXeT4A', '2022-12-27 01:20:00', '2022-12-27 01:20:00'),
-('548983', 'Hanustavira Guru Acarya', 'hanustavira.acarya@binus.ac.id', '6285745402100', 'Singosari', '$2y$10$n6tFXxA55MdrW7sGMB5EGe3PIIAXo5FCxcOcpolMALGtsryw/GVq2', NULL, 'K0eCgKKoIoGFL8tp', '2022-12-27 01:20:00', '2022-12-27 01:20:00'),
-('587341', 'Fauzan Mustofa', 'fauzanmusthofa.fm@gmail.com', '6281111116098', 'Sawojajar', '$2y$10$C3Yj6nH44GRAPrjr724zjumw1QgduFMNrUzIVRXT3Vy1LHAiKSUtC', NULL, '71vYubkl91AEgbi5', '2022-12-27 01:20:00', '2022-12-27 01:20:00'),
-('666227', 'Viana Salsabila Tauda', 'vianatauda@gmail.com', '6281344847038', 'Sawojajar', '$2y$10$MeYc1QwUl0K.2e0UhXvefOm9shsKhmVK.tY2BaB3h2k1hG.ejYM1.', NULL, 'OlP3cgGJDDAjngqa', '2022-12-27 01:20:00', '2022-12-27 01:20:00'),
-('903243', 'Thomas Harman Bintang', 'thomasharman99@gmail.com', '6285171191215', 'Blimbing', '$2y$10$fNeSlBb.UUmGSmnsDq8G1.L7uKwPFFACh7FwGBOjl/sB81yuSpqqC', NULL, 'Wb8l8dle9lJB1Ndh', '2022-12-27 01:20:00', '2022-12-27 01:20:00');
+('249618', 'Achmad Agil Susanto', 'ahmadagilsusanto@gmail.com', '6282237174435', 'Sawojajar', '$2y$10$PyYqYSZqvEzVq/gEPWSAa.JP9Zn1reM9NjbWNmbVKXS555tOqhFB6', NULL, 'COlpQPKh4ZgZTogV', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('399978', 'Fauzan Mustofa', 'fauzanmusthofa.fm@gmail.com', '6281111116098', 'Sawojajar', '$2y$10$YNvZMfs9Mbnyzj.bXtlP6u2DjdN05.oZI787kepC1f2IwkXAlmdBu', NULL, 'TD9zsrg1P1gU4Bt3', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('707784', 'Thomas Harman Bintang', 'thomasharman99@gmail.com', '6285171191215', 'Blimbing', '$2y$10$lEC6ctepkB8WBb4pwYa8vu4wAaSf3c0ovz/q9LhLEa0NdjMBwPxLS', NULL, 'yIGzAnNeLVxzv5AC', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('882536', 'Hanustavira Guru Acarya', 'hanustavira.acarya@binus.ac.id', '6285745402100', 'Singosari', '$2y$10$IBkSmx0O7ppueX6.QHYvjOOWJc0V0rGHurIUJK6oDZOxuQpYI8/yC', NULL, 'glDHUMb6HPFpHDg4', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('981474', 'Viana Salsabila Tauda', 'vianatauda@gmail.com', '6281344847038', 'Sawojajar', '$2y$10$/1BeEBFxQQwFXHuqAWy8meWNKt92eb4UcbYrDO5zBcq0CbT/vaPcW', NULL, 'QUYZScZdLrHTxSDv', '2023-01-09 20:12:20', '2023-01-09 20:12:20');
 
 -- --------------------------------------------------------
 
@@ -288,11 +294,11 @@ CREATE TABLE `wallet` (
 --
 
 INSERT INTO `wallet` (`id`, `saldo`, `created_at`, `updated_at`) VALUES
-('548983', '50000.00', '2022-12-27 01:20:00', '2022-12-27 01:20:00'),
-('587341', '150000.00', '2022-12-27 01:20:00', '2022-12-27 01:20:00'),
-('666227', '75000.00', '2022-12-27 01:20:00', '2022-12-27 01:20:00'),
-('903243', '90000.00', '2022-12-27 01:20:00', '2022-12-27 01:20:00'),
-('449217', '65000.00', '2022-12-27 01:20:00', '2022-12-27 01:20:00');
+('882536', '50000.00', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('399978', '150000.00', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('981474', '75000.00', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('707784', '90000.00', '2023-01-09 20:12:20', '2023-01-09 20:12:20'),
+('249618', '65000.00', '2023-01-09 20:12:20', '2023-01-09 20:12:20');
 
 --
 -- Indexes for dumped tables
@@ -389,7 +395,7 @@ ALTER TABLE `wallet`
 -- AUTO_INCREMENT untuk tabel `isi_refill`
 --
 ALTER TABLE `isi_refill`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `jenis_refill_riwayat`
@@ -407,7 +413,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT untuk tabel `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `riwayat_topup`
